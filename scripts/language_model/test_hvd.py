f5f2150db1631143627096b71dd2a9f462f40513
cx2 <cx2@illinois.edu>
2019-08-29 15:55:51 -0500

    sparse adaalter

--- scripts/language_model/test_hvd.py
+++ scripts/language_model/test_hvd.py
@@ -79,6 +79,7 @@ logging.info(x_row_sparse.indices.shape)
 
 n_entres = mx.nd.array(x_row_sparse.indices.shape[0])
 
+logging.info(n_entres)
 hvd.allreduce_(n_entres, average=False)
 mx.nd.waitall()
 logging.info(n_entres)
