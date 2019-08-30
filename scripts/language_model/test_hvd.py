ae48343eb6ae4f01903eb24231f9889c904daac1
cx2 <cx2@illinois.edu>
2019-08-30 18:18:39 -0500

    test sparse

--- scripts/language_model/test_hvd.py
+++ scripts/language_model/test_hvd.py
@@ -75,9 +75,8 @@ x[0, rank] = 2
 x_row_sparse = x.tostype('row_sparse')
 mx.nd.waitall()
 
-a = mx.nd.array([x_row_sparse.indices.shape[0]])
-logging.info(a)
+logging.info(x)
 
-hvd.allreduce_(a, average=False)
+y = hvd.allgather(x)
 mx.nd.waitall()
-logging.info(a)
+logging.info(y)
