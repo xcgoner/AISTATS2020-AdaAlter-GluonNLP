2563b7f650c9aa0531dea146adf1a6ce9c841aa0
cx2 <cx2@illinois.edu>
2019-09-26 22:46:50 -0500

    debug

--- scripts/language_model/distributed_local_sgd_v4.py
+++ scripts/language_model/distributed_local_sgd_v4.py
@@ -129,6 +129,7 @@ class DistributedHierLocalHVDTrainer(mx.gluon.Trainer):
                 if param._stype == 'default':
                     hvd.allreduce_(state_array, average=True, 
                                    name=str(idx), priority=i-len(self._params)*2)
+                    self._updaters[0].states[i][0][:] = state_array
                     # self._updaters[0].states[i][0][:] += mx.nd.square(state_array)
                     # state_array[:] = 0
                 else:
@@ -142,4 +143,3 @@ class DistributedHierLocalHVDTrainer(mx.gluon.Trainer):
         for i, param in reversed(list(enumerate(self._params))):
             if param.grad_req != 'null':
                 self._updaters[0].states[i] = (self._updaters[0].states[i][0], mx.nd.zeros_like(self._updaters[0].states[i][0]))
-                # self._updaters[0].states[i] = (self._updaters[0].states[i][0], self._updaters[0].states[i][0].copy())
