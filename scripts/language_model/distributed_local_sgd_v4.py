e0bcfa7fc4a4d8956a8c8da42906614e5bd50b76
cx2 <cx2@illinois.edu>
2019-09-26 23:07:03 -0500

    debug

--- scripts/language_model/distributed_local_sgd_v4.py
+++ scripts/language_model/distributed_local_sgd_v4.py
@@ -131,8 +131,7 @@ class DistributedHierLocalHVDTrainer(mx.gluon.Trainer):
                                    name=str(idx), priority=i-len(self._params)*2)
                     mx.nd.square(state_array, out=state_array)
                     self._updaters[0].states[i][0][:] += state_array
-                    # self._updaters[0].states[i][0][:] += mx.nd.square(state_array)
-                    # state_array[:] = 0
+                    state_array[:] = 0
                 else:
                     raise ValueError("Cannot pull row_sparse parameters for local SGD")
 
